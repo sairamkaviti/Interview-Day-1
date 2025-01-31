7daf64cefa98aa04e4c7d606d9a5768a74b9bf72
sairam <sairamkaviti1995@gmail.com>
2025-01-31 17:40:35 +0530

    typeof operator checked

--- script.js
+++ script.js
@@ -21,3 +21,47 @@ function blockScope() {
     //  console.log(z);
 }
 blockScope();
+
+
+
+// dbw null and undefined :
+
+let unVariable;
+console.log(unVariable); // o/p:undefined;
+
+let nullVariable = null;
+console.log(nullVariable);// o/p:null;
+
+// switch case :
+
+let day = "monday";
+switch (day){
+    case "sunday":
+        console.log("Jesus");
+        
+        break;
+    case "tuesday":
+        console.log("Jai Hanuman");
+       
+        break;
+    default: console.log("Hara Hara Shiva");
+}
+
+// typeof operator :
+
+let a1 = +"10";
+let a2 = 20;
+let a3 = true;
+let a4 = { name: "sai", age: 28 };
+let a5 = [11, 22, 33];
+let a6 = unVariable;
+let a7 = nullVariable;
+console.log(typeof a1);
+console.log(typeof a2);
+console.log(typeof a3);
+console.log(typeof a4);
+console.log(typeof a5);
+console.log(typeof a6);
+console.log(typeof a7);
+
+console.log([]+ {})
