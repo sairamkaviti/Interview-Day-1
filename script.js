58c8d0e866aaa7a31354a4e5b0e7799f636b700f
sairam <sairamkaviti1995@gmail.com>
2025-01-31 16:46:35 +0530

    var let const code

--- script.js
+++ script.js
@@ -1,2 +1,23 @@
+// function scope :
+function checking() {
+    var x = "fns varibale";
+    if (true) {
+        console.log(x);
+    }
+    console.log(x)
+}
+checking();
 
+// block scope :
+function blockScope() {
+    if (true) {
+        let y = "sai";
+        const z = "suppuuu";
+        console.log(y);
+        console.log(z)
 
+    }
+    //  console.log(y); not accessable here beacause block scope;
+    //  console.log(z);
+}
+blockScope();
